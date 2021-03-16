f6fd21ef64f71c6029b1f806bbb1d6a7b2680ac6
mlibre <m.gh@linux.org>
2021-03-16 19:30:36 +0330

    new posts

---
+++ users/mlibre/posts/How to set nano tab size to 4/post.js
@@ -0,0 +1,35 @@
+module.exports =
+{
+	type: "video",
+	title: "How to set nano tab size to 4",
+	address: "video.mp4",
+	cover: "terminal.png",
+	body: "How to change nano tab size from 8 to 4. Linux terminal.\n" +
+    "Commands:\n" +
+    "sudo nano /etc/nanorc\n" +
+    "set tabsize 4",
+	githubFileName: "How to set nano tab size to 4",
+	refrences: [ "https://lbry.tv/@mlibre:e/how-to-set-nano-tab-size-to-4:2" ],
+	youtube: "https://youtu.be/Oulw7DYzoqw",
+	donate: { "My ETH Address:": "0xc9b64496986E7b6D4A68fDF69eF132A35e91838e" },
+	tags: [ "linux", "terminal", "tutorial" ],
+	minds: {
+		status: true,
+		link: "https://www.minds.com/mlibre/blog/how-to-set-nano-tab-size-to-4-1218585366102642688"
+	},
+	gab: {
+		status: true,
+		link: "https://gab.com/mlibre/posts/105900245491963674"
+	},
+	wordpress: {
+		status: true,
+		link: "https://mlibrego.wordpress.com/2021/03/16/how-to-set-nano-tab-size-to-4/"
+	},
+	github: {
+		status: true,
+		link: "https://github.com/mlibre/contents/blob/master/posts/How to set nano tab size to 4.md"
+	},
+	filepath: "users/mlibre/posts/How to set nano tab size to 4/post.js",
+	filename: "post.js",
+	folderPath: "users/mlibre/posts/How to set nano tab size to 4/"
+};
